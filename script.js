5b4249b1368071433bf417cff169d2e860fa4d99
sri-11-04 <srianna6778@gmail.com>
2025-04-02 10:50:42 +0530

    updated resume and intersections added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-let root = null,
-    div = document.getElementById(`div`),
+let root = document.documentElement
+let div = document.getElementById(`div`),
     obj = {
         root:null,
         threshold:1,
@@ -8,17 +8,56 @@ let root = null,
     }
 
     let observer = new IntersectionObserver((entries)=>{
-        console.log(entries)
+        if (true){
+            console.log(getComputedStyle(root).getPropertyValue('--percentage'))
+        setTimeout(complete,500)
+        // complete()
+        function success(){
+            return new Promise((res)=>{
+            const percent = async () =>{
+                if(getComputedStyle(root).getPropertyValue('--percentage').trim()==='100%'){
+                    res()
+                }
+                else{
+                    setTimeout(percent,500)
+                }
+            }
+                percent()
+            })
+        }
+
+        function increase(){
+            let progress = 0
+            const progInterval = setInterval(()=>{
+                progress++
+                root.style.setProperty('--percentage',`${progress}%`)          //* before conical bg
+                root.style.setProperty('--hello',`'${progress}% ✔️'`)          //* before conical bg
+                if (progress>=100){
+                    clearInterval(progInterval)
+                }
+            },10)
+        }
+
+        async function complete() {
+            increase()
+            await success()
+        }
+        }else{
+            root.style.setProperty('--percentage',`0%`)
+        }
+        
+
+        
     },obj)
 
-const inter = setInterval(()=>{
-    if (screen.width <= 469){
-        observer.observe(div)
-        clearInterval(inter)
-    }
-},500)
+// const inter = setInterval(()=>{
+//     if (screen.width <= 469){
+//         observer.observe(div)
+//         clearInterval(inter)
+//     }
+// },500)
     
-console.log(screen.width)
+// console.log(screen.width)
 
 
 
